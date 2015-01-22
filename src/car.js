39760b119d3cf19696dac657022c157443847760
Cynthia Yeung <cyeung91@gmail.com>
2015-01-22 09:46:03 -0800

    Completed homework

--- src/car.js
+++ src/car.js
@@ -1,13 +1,62 @@
 function Car(make, model, year, color){
-
+	this.make = make;
+	this.model = model;
+	this.year = year;
+	this.state = "off";
+	//this.state = state;
+	this.color = color;
+	this.previous_owners = [];
+	this.current_owner = "Manufacturer";
+	this.passengers = [];
+	// this.previousOwners = previousOwners;
+	// this.currentOwners = currentOwners;
 }
 
-Car.prototype.sale = function(newOwner){
+// var car1 = new Car("BMW", "X5", 2012, "Red");
 
-};
+Car.prototype.sale = function(newOwner){
+	this.previous_owners.push(this.current_owner);
+	this.current_owner = newOwner;
+}
 
 Car.prototype.paint = function(newColor){
+	this.color = newColor;
+}
 
-};
+Car.prototype.start = function() {
+	this.state = "on";
+}
+
+Car.prototype.off = function(){
+	this.state = "off";
+}
+
+Car.prototype.driveTo = function(destination) {
+	if(this.state == "on") {
+		console.log("driving to " + destination);
+	}
+}
+
+Car.prototype.park = function() {
+	if(this.state == "off"){
+		console.log("parked!!");
+	}
+}
+
+//Car.prototype.passengers = [];
+
+Car.prototype.pick_up = function(name) {
+	if(this.state === "on") {
+		this.passengers.push(name);
+		console.log("driving to pick up " + name)
+	}
+}
+
+Car.prototype.dropOff = function(name) {
+	if(this.state === "on"){
+		// this.passengers.pop(name);
+		this.passengers.splice(this.passengers.indexOf(name),1) 
+	}
+}
 
 module.exports=Car;
